a411d5bd37811c991fe647e36430f83595124f72
somya143 <maaheersj@gmail.com>
2023-06-17 14:31:37 +0530

    done auth redux

--- frontend/src/redux/auth/auth.action.js
+++ frontend/src/redux/auth/auth.action.js
@@ -1,10 +1,28 @@
+import { auth_login_failure, auth_login_loading, auth_login_success, auth_register_failure, auth_register_loading, auth_register_success, auth_signout } from "./auth.actionType"
+import axios from "axios";
 
-
-export const getRegister = () => async(dispatch) => {
-    
+export const getRegister = (cred) => async(dispatch) => {
+    dispatch({type: auth_register_loading})
     try {
-        
+    const response = await axios.post("http://localhost:8080/auth/signup", cred);
+    dispatch({type: auth_register_success , payload : response.data});
+    return response.data; 
     } catch (error) {
-        
+        dispatch({type: auth_register_failure , payload : error.message})
     }
+}
+
+export const getLogin = (Credential) => async(dispatch) => {
+    dispatch({type: auth_login_loading});
+    try {
+    const response = await axios.post("http://localhost:8080/auth/login", Credential);
+    dispatch({type: auth_login_success , payload: response.data});
+    return response.data;    
+    } catch (error) {
+        dispatch({type: auth_login_failure , payload : error.message})
+    }
+}
+
+export const getSignout = () => {
+    return ({type: auth_signout})
 }
