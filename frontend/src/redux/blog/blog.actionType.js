46a9cc7791a7a37099e00d0e2d9d640cb00a547d
somya143 <maaheersj@gmail.com>
2023-06-21 12:03:06 +0530

    added blog reducer function in redux

--- frontend/src/redux/blog/blog.actionType.js
+++ frontend/src/redux/blog/blog.actionType.js
@@ -1,3 +1,15 @@
-export const blog_loading = "blog/blog_loading";
-export const blog_success = "blog/blog_success";
-export const blog_failure = "blog/blog_failure";
+export const get_blog_loading = "blog/get/blog_loading";
+export const get_blog_success = "blog/get/blog_success";
+export const get_blog_failure = "blog/get/blog_failure";
+
+export const post_blog_loading = "blog/post/blog_loading";
+export const post_blog_success = "blog/post/blog_success";
+export const post_blog_failure = "blog/post/blog_failure";
+
+export const delete_blog_loading = "blog/delete/blog_loading";
+export const delete_blog_success = "blog/delete/blog_success";
+export const delete_blog_failure = "blog/delete/blog_failure";
+
+export const update_blog_loadng = "blog/update/blog_loading";
+export const update_blog_success = "blog/update/blog_success";
+export const update_blog_failure = "blog/update/blog_failure";
--- frontend/src/redux/blog/blogReducer.js
+++ frontend/src/redux/blog/blogReducer.js
@@ -1,4 +1,4 @@
-import { blog_failure, blog_loading, blog_success } from "./blog.actionType"
+import { delete_blog_failure, delete_blog_loading, delete_blog_success, get_blog_failure, get_blog_loading, get_blog_success, post_blog_failure, post_blog_loading, post_blog_success, update_blog_failure, update_blog_loadng, update_blog_success } from "./blog.actionType"
 
 const initialBlogState = {
     blog : [],
@@ -8,14 +8,14 @@ const initialBlogState = {
 
 export const blogReducer = (state= initialBlogState , {type,payload}) => {
     switch(type){
-        case blog_loading : {
+        case get_blog_loading : {
             return {
                 ...state,
                 isLoading : true,
                 isError : false
             }
         }
-        case blog_success : {
+        case get_blog_success : {
             return {
                 ...state,
                 blog : payload,
@@ -23,10 +23,73 @@ export const blogReducer = (state= initialBlogState , {type,payload}) => {
                 isError : false
             }
         }
-        case blog_failure : {
+        case get_blog_failure : {
             return {
                 ...state,
                 isError: true,
+                isLoading : false
+            }
+        }
+        case post_blog_loading : {
+            return {
+                ...state,
+                isError : false,
+                isLoading : true,
+            }
+        }
+        case post_blog_success : {
+            return {
+                ...state,
+                isError : false,
+                blog : [payload,...state.blog]
+            }
+        }
+        case post_blog_failure : {
+            return {
+                ...state,
+                isError : true,
+                isLoading : false
+            }
+        }
+        case delete_blog_loading : {
+            return {
+                ...state,
+                isError : false,
+                isLoading : true
+            }
+        }
+        case delete_blog_success : {
+            return {
+                ...state,
+                isError : false,
+                isLoading : false,
+                blog : state.blog.filter(el => el._id!==payload )
+            }
+        }
+        case delete_blog_failure : {
+            return {
+                ...state,
+                isError : true,
+                isLoading : false
+            }
+        }
+        case update_blog_loadng : {
+            return {
+                ...state,
+                isError : false,
+                isLoading : true,
+            }
+        }
+        case update_blog_success : {
+            return {
+                isError : false,
+                isLoading : false,
+                blog : state.blog.map(el => el._id===payload._id? payload:el)
+            }
+        }
+        case update_blog_failure : {
+            return {
+                isError : true,
                 isLoading : false
             }
         }
