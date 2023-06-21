213a87c4c9b055b7bc4bebc51f0cfc7723969f28
somya143 <maaheersj@gmail.com>
2023-06-21 14:54:38 +0530

    added fetching in redux

--- frontend/src/redux/blog/blog.action.js
+++ frontend/src/redux/blog/blog.action.js
@@ -1,26 +1,26 @@
 import axios from "axios";
-import { blog_failure, blog_loading, blog_success } from "./blog.actionType";
+import { delete_blog_loading, delete_blog_success, get_blog_failure, get_blog_loading, get_blog_success, post_blog_failure, post_blog_loading, post_blog_success, update_blog_loadng } from "./blog.actionType";
 
 
 export const getBlog = () =>  async(dispatch) => {
-    dispatch({ type: blog_loading});
+    dispatch({ type: get_blog_loading});
     try {
         const response = await axios.get("http://localhost:8080/blogs");
-        dispatch({type: blog_success , payload : response.data});
+        dispatch({type: get_blog_success , payload : response.data});
         return response.data
     } catch (error) {
-        dispatch({type: blog_failure , payload: error.message})
+        dispatch({type: get_blog_failure , payload: error.message})
     }
 }
 
 export const createBlog = () => async(dispatch) => {
-    dispatch({ type: blog_loading });
+    dispatch({ type: post_blog_loading });
     try {
         const response = await axios.post("http://localhost:8080/blogs");
-        dispatch({ type: blog_success , payload: response.data});
+        dispatch({ type: post_blog_success , payload: response.data});
         return response.data;
     } catch (error) {
-        dispatch({ type: blog_failure , payload: error.message });
+        dispatch({ type: post_blog_failure , payload: error.message });
     }
 }
 
@@ -36,12 +36,21 @@ export const getSingleBlog = (id) => async(dispatch) => {
 }
 
 export const deleteBlog = (id) => async(dispatch) => {
-    dispatch({ type:blog_loading });
+    dispatch({ type: delete_blog_loading });
     try {
         const response = await axios.delete(`http://localhost:8080/blogs/${id}`);
-        dispatch({type: blog_success , payload: response.data });
+        dispatch({type: delete_blog_success , payload: response.data });
         return response.data
     } catch (error) {
-        dispatch({ type: blog_failure , payload: error.message})
+        dispatch({ type: delete_blog_success , payload: error.message})
+    }
+}
+
+export const updateBlog = () => async(dispatch) => {
+    dispatch({ type: update_blog_loadng });
+    try {
+        const response = await axios.
+    } catch (error) {
+        
     }
 }
