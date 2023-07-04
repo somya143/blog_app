ced3ccd2745c91a9e036f29a137a2633f7d4997c
somya143 <maaheersj@gmail.com>
2023-07-04 18:59:09 +0530

    some changes in auth pages

--- backend/src/features/auth/auth.routes.js
+++ backend/src/features/auth/auth.routes.js
@@ -4,15 +4,22 @@ const jwt = require("jsonwebtoken");
 const argon2 = require("argon2");
 const Auth = require("./auth.model");
 
-// app.post("/refresh" , async(req,res) => {
-//     try {
-//         const refreshToken = req.body;
-//         const verification = jwt.verify(refreshToken,"SECRET@1991");
-//         const user = await Auth.findOne({email:verification.email});
-//     } catch (error) {
-        
-//     }
-// })
+app.post("/refresh", async (req, res) => {
+    // redis.push(token)
+    try {
+      const { refreshToken } = req.body;
+      const verification = jwt.verify(refreshToken, "REFRESH_TOKEN_SECRET@1991");
+      const user = await Auth.findOne({ email: verification.email });
+      const { email, name, password } = user;
+      const token = jwt.sign({ name, email, password }, "SECRET@1991", {
+        expiresIn: "5 minutes",
+      });
+      res.send({ error: false, token, message: "new token generated" });
+    } catch (e) {
+      // redis.push(rToken)
+      res.send({ error: true, message: e.message });
+    }
+  });
 
 app.post("/signup" , async(req,res) => {
     const { name,email,password,age} = req.body;
@@ -39,20 +46,36 @@ app.post("/signup" , async(req,res) => {
 })
 
 app.post("/login" , async(req,res) => {
-    const { email , password } = req.body;
-    try {
-        const user = await Auth.findOne({email})
-        let matched = await argon2.verify(user.password,password);
-        if(matched){
-         const token = jwt.sign({id:user.id , name:user.name , email:user.email , password:user.password , role:user.role}, 
-            "SECRET@1991" , {expiresIn: "7 days"});
-          const refreshToken = jwt.sign({id:user.id , name:user.name , email:user.email , password:user.password , role:user.role}, 
-            "REFRESH_TOKEN_SECRET@1991" , {expiresIn: "28 days"}) ;
-            return res.status(201).send({message:"Token send successfully" , token, refreshToken})
+    const { email , password } = req.body
+  try {
+    const user = await Auth.findOne({ email });
+    const match = await argon2.verify(user.password, password);
+    if (match) {
+      const token = jwt.sign(
+        { id: user._id, email, password: user.password, name: user.name },
+        "SECRET@1991",
+        {
+          expiresIn: "7 days",
         }
-    } catch (error) {
-        return res.status(401).send(error.message)
+      );
+      const refreshToken = jwt.sign(
+        { id: user._id, email, password: user.password, name: user.name },
+        "REFRESH_TOKEN_SECRET@1991",
+        { expiresIn: "28 days" }
+      );
+      return res.send({
+        error: false,
+        message: "sign in successful",
+        name: user.name,
+        token,
+        refreshToken,
+      });
+    } else {
+      return res.send({ error: true, message: "Wrong Password!!" });
     }
+  } catch (e) {
+    res.send({ error: true, message: "Wrong Credential!!" });
+  }
 })
 
 module.exports = app;
